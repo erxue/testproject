8b1d59a00c3b1b5832776f4d784f9d481ebd50d1
Eric Xue <exue1696@gmail.com>
2016-04-12 10:31:53 -0700

    newer2

--- app/src/main/java/com/android/testproject/MainActivity.java
+++ app/src/main/java/com/android/testproject/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
-        //blah blah blah
+        // blah blah blah
 
         return super.onOptionsItemSelected(item);
     }
